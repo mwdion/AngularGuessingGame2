a70d928ab206859e4456c50f94270ab85cd599b5
Michael Dion <mwdion@gmail.com>
2015-02-18 12:55:14 -0500

    Random number function is fixed.

--- app.js
+++ app.js
@@ -1,44 +1,49 @@
+function getRandomNumber(upper) {
+    return Math.floor((Math.random() * upper) + 1);
+}
 (function(){
   var app = angular.module('guessGame', []);
 
 app.controller('GameController', function($scope, $timeout){
+  var randomNumber;
   this.playerName = ' ';
   this.upper = ' ';
+  this.randomNumber = getRandomNumber();
   this.state = 1; // start screen
   this.guessCount = 0;
-  this.randomNumber = ' ';
   this.flash = '';
 
-  // this.setUpper = function(newUpper){
-  //   this.upper = newUpper;
-  // };
+  this.updateUpper = function(upper){
+    this.upper = upper;
+    randomNumber = getRandomNumber(upper);
+    console.log(upper);
+    console.log(randomNumber);
+  };
 
-  // this.getRandomNumber = function(upper){
-  //   this.upper = upper;
-  //   return Math.floor(Math.random() * upper) + 1;
-  // };
   
   this.setState = function(newState) {
             this.state = newState;
             this.playerGuess = 0;
+            console.log($scope.upper);
 
             if(newState == 3) { // flash screen
                 $timeout(function() {
                     $scope.game.state = 2; // go back to guessing
-                }, 3000);
+                }, 2250);
             }
 
             if(newState == 4) { // won
                 $timeout(function() {
                     $scope.game.resetGame();
-                }, 3000);
+                }, 2250);
                 console.log(this.playerGuess);
             }
         };
   this.guess = function() {
             var parsedGuess = parseInt(this.playerGuess);
+            this.randomNumber = randomNumber;
             // guessing logic
-            console.log(this.randomNumber);
+            console.log(randomNumber);
             if(parsedGuess == this.randomNumber) {
                 this.flash = 'You got it!';
                 this.setState(4);
@@ -54,7 +59,7 @@ app.controller('GameController', function($scope, $timeout){
   this.resetGame = function() {
             this.state = 1;
             this.playerGuess = 0;
-            this.randomNumber = getRandomNumber(upper);
+            console.log($scope.upper);
             this.flash = '';
         };
 
